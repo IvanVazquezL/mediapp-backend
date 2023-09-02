7838e88a7ffbd5f200bb095cfebc8a5794114719
IvanVazquezL <ivanvl_11@hotmail.com>
2023-09-01 21:54:26 -0600

    Customize error with ErrorResponse

--- src/main/java/com/ivan/mediappbackend/exception/ResponseExceptionHandler.java
+++ src/main/java/com/ivan/mediappbackend/exception/ResponseExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -16,13 +17,13 @@ import java.time.LocalDateTime;
 public class ResponseExceptionHandler {
     // Since Spring Boot 3
     @ExceptionHandler(ModelNotFoundException.class)
-    public ProblemDetail handleModelNotFoundException(ModelNotFoundException ex, WebRequest request) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("Model Not Found Exception");
-        problemDetail.setType(URI.create("/not-found"));
-        problemDetail.setProperty("extra1", "extra-value");
-        problemDetail.setProperty("extra2", 32);
-        return problemDetail;
+    public ErrorResponse handleModelNotFoundException(ModelNotFoundException ex, WebRequest request) {
+        return ErrorResponse.builder(ex, HttpStatus.NOT_FOUND, ex.getMessage())
+                .title("Model Not Found")
+                .type(URI.create(request.getContextPath()))
+                .property("extra1", "extravalue")
+                .property("extra2", 32)
+                .build();
     }
 
 }
