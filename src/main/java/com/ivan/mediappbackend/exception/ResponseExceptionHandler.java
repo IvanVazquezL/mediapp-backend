b9488258b5d7aac5e667be5e32e7a9535087166a
IvanVazquezL <ivanvl_11@hotmail.com>
2023-09-01 21:50:41 -0600

    Customize error with Problem detail

--- src/main/java/com/ivan/mediappbackend/exception/ResponseExceptionHandler.java
+++ src/main/java/com/ivan/mediappbackend/exception/ResponseExceptionHandler.java
@@ -1,19 +1,28 @@
 package com.ivan.mediappbackend.exception;
 
+import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 
 // To send our customized exceptions
 @RestControllerAdvice
 public class ResponseExceptionHandler {
+    // Since Spring Boot 3
     @ExceptionHandler(ModelNotFoundException.class)
-    public ResponseEntity<CustomErrorResponse> handleModelNotFoundException(ModelNotFoundException ex, WebRequest request) {
-        CustomErrorResponse err = new CustomErrorResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    public ProblemDetail handleModelNotFoundException(ModelNotFoundException ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Model Not Found Exception");
+        problemDetail.setType(URI.create("/not-found"));
+        problemDetail.setProperty("extra1", "extra-value");
+        problemDetail.setProperty("extra2", 32);
+        return problemDetail;
     }
+
 }
