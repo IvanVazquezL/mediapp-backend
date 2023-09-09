05a9fa05b4a6715d85284c0164eaed36d412fac2
IvanVazquezL <ivanvl_11@hotmail.com>
2023-09-08 19:14:53 -0600

    Repo for Exam and Specialty added

---
+++ src/main/java/com/ivan/mediappbackend/repo/IExamRepo.java
@@ -0,0 +1,6 @@
+package com.ivan.mediappbackend.repo;
+
+import com.ivan.mediappbackend.model.Exam;
+
+public interface IExamRepo extends IGenericRepo<Exam, Integer> {
+}
---
+++ src/main/java/com/ivan/mediappbackend/repo/ISpecialtyRepo.java
@@ -0,0 +1,6 @@
+package com.ivan.mediappbackend.repo;
+
+import com.ivan.mediappbackend.model.Specialty;
+
+public interface ISpecialtyRepo extends IGenericRepo<Specialty, Integer> {
+}
